a7bf21cd698bcdd4cb21176b72023ac1718a7e05
WendyOmollo <omollow280@gmail.com>
2019-10-09 17:38:31 +0300

    Add an interface and another class

--- app/src/main/java/com/moringaschool/myrestaurants/YelpApi.java
+++ app/src/main/java/com/moringaschool/myrestaurants/YelpApi.java
@@ -1,4 +1,9 @@
 package com.moringaschool.myrestaurants;
 
 public interface YelpApi {
+    @GET("businesses/search")
+    Call<YelpBusinessSearchResponse> getRestaurants(
+            @Query("location") String location,
+            @Query("term") String term
+    );
 }
